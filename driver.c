8bfd4106bc5464ce6fadae9bc98498e76742e408
Pavel Desyatnikov <desyatnikovpvl@gmail.com>
2024-04-01 22:22:41 +0300

    Write a dummy device for open/close only ops and add small test for that
    Signed-off-by: Pavel Desyatnikov <desyatnikovpvl@gmail.com>

--- driver.c
+++ driver.c
@@ -1,20 +1,82 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/types.h>
+#include <linux/atomic.h>
+#include <linux/cdev.h>
+#include <linux/fs.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("desyatok");
+MODULE_DESCRIPTION("A sample driver");
 
 #define SUCCESS 0
 #define FAILURE -1
+#define DEVICE_NAME "aes_p"
+
+static int device_open(struct inode *, struct file *);
+static int device_release(struct inode *, struct file *);
+
+static int major;
+
+enum {
+	DEV_NOT_USED = 0,
+	DEV_EXCLUSIVE_OPEN = 1,
+};
+
+static atomic_t already_open = ATOMIC_INIT(DEV_NOT_USED);
+
+static struct class *cls;
+
+static struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.open = device_open,
+	.release = device_release,
+};
 
 static int __init my_init(void) {
-	pr_info("hello\n");
+	major = register_chrdev(0, DEVICE_NAME, &fops);
+	
+	if (major < 0) {
+		pr_alert("Registering a device failed with %d\n", major);
+		return major;
+	}
+
+	pr_info("Device was assigned major number %d\n", major);
+
+	cls = class_create(DEVICE_NAME);
+	device_create(cls, NULL, MKDEV(major, 0), NULL, DEVICE_NAME);
+
+	pr_info("Device was created on /dev/%s\n", DEVICE_NAME);
+
 	return SUCCESS;
 }
 
 static void __exit my_exit(void) {
-	pr_info("bye\n");
+	device_destroy(cls, MKDEV(major, 0));
+	class_destroy(cls);
+	unregister_chrdev(major, DEVICE_NAME);
+	pr_info("Device was successfully destroyed and unregistered\n");
+}
+
+static int device_open(struct inode *inode, struct file *file) {
+	if (atomic_cmpxchg(&already_open, DEV_NOT_USED, DEV_EXCLUSIVE_OPEN)) {
+		return -EBUSY;
+	}
+
+	pr_info("Successfully opened a device\n");
+	try_module_get(THIS_MODULE);
+
+	return SUCCESS;
+}
+
+static int device_release(struct inode *inode, struct file *file) {
+	atomic_set(&already_open, DEV_NOT_USED);
+	module_put(THIS_MODULE);
+	pr_info("Successfully closed a device");
+	return SUCCESS;
 }
 
 module_init(my_init);
 module_exit(my_exit);
-
-MODULE_LICENSE("GPL");
 
---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main() {
+	int dev = open("/dev/aes_p", O_RDONLY);
+	if (dev == -1) {
+		printf("No success\n");
+		return -1;
+	}
+	printf("success\n");
+	close(dev);
+	return 0;
+}
+
